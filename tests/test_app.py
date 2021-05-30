72fb0ec48bbe4648acf3515f684f4e831de15cec
Sam Szuflita <sam.szuflita@gmail.com>
2021-05-30 15:07:21 -0400

    Reasonable names

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,13 @@
 from datetime import timedelta
 import time
-from app.app import app, store_metric, sum
+import app.main
 
-app.window = timedelta(seconds=1)
+app.main.window = timedelta(seconds=1)
 
 
 def test_window():
 
-    client = app.test_client()
+    client = app.main.app.test_client()
 
     response = client.get('/metric/asdf/sum').get_json()
 
@@ -23,9 +23,28 @@ def test_window():
     assert 'value' in response
     assert response['value'] == 10
 
-    time.sleep(6)
+    time.sleep(2)
 
     response = client.get('/metric/asdf/sum').get_json()
 
     assert 'value' in response
+    assert response['value'] == 0
+
+
+def test_purge():
+    client = app.main.app.test_client()
+
+    client.post('/metric/asdf', json={'value': 10}).get_json()
+    client.post('/metric/asdf', json={'value': 10}).get_json()
+    time.sleep(0.5)
+    client.post('/metric/asdf', json={'value': 10}).get_json()
+    response = client.get('/metric/asdf/sum').get_json()
+    assert response['value'] == 30
+    time.sleep(0.6)
+    
+    response = client.get('/metric/asdf/sum').get_json()
+    assert response['value'] == 10
+
+    time.sleep(0.5)
+    response = client.get('/metric/asdf/sum').get_json()
     assert response['value'] == 0
